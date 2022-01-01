c7a54d6c3914c43da53ab3f97c0ab56de2abfd85
Vie <the_ruesi@zoho.com>
2022-01-01 23:22:14 +0000

    init commit2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Vie Base 2022</h1>
+      <p>Welcome to Vie base,  currently in build!</p>
     </div>
   );
 }
